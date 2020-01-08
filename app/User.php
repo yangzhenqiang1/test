91ad9adf6e92952644338c8ecbbc3ee0bf43b890
yangzhenqiang <1285886507@qq.com>
2020-01-08 17:22:14 +0800

    dev test

--- app/User.php
+++ app/User.php
@@ -13,6 +13,7 @@ class User extends Authenticatable
      * The attributes that are mass assignable.
      *
      * @var array
+     * 
      */
     protected $fillable = [
         'name', 'email', 'password',
