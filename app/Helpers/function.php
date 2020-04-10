9a272a74ae6a420643e63584fc5f72b631b9b5d2
yangzhenqiang <1285886507@qq.com>
2020-04-10 09:53:58 +0800

    common

---
+++ app/Helpers/function.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 公用的方法  返回json数据，进行信息的提示
+ * @param $status 状态
+ * @param string $message 提示信息
+ * @param array $data 返回数据
+ */
+function showMsg($status,$message = '',$data = array()){
+    $result = array(
+        'status' => $status,
+        'message' =>$message,
+        'data' =>$data
+    );
+    exit(json_encode($result));
+}
